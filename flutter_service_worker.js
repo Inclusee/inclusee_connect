1416bfe200b6530ea048ade2fc509c4bb36f8151
kalisynth <cade2002@gmail.com>
2023-02-21 11:18:27 +1000

    ad update icon update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -193,10 +193,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "23e3d5fe7d4782517745bc209c2b9bc1",
-"/": "23e3d5fe7d4782517745bc209c2b9bc1",
+"index.html": "27cea7a157782be588ca2f5b272dff68",
+"/": "27cea7a157782be588ca2f5b272dff68",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "4db7a09c757a3004e876ba0dc6f54f19",
+"main.dart.js": "32fdfc412d852bfa06f25b8894affe0f",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
