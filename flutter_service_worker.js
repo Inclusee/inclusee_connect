719494756ce779420204fb192b769271a2f4ff4b
kalisynth <cade2002@gmail.com>
2023-05-18 10:51:46 +1000

    vow adjusments

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -224,10 +224,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "d53f6a9e69b4486b678de60ef89ad942",
-"/": "d53f6a9e69b4486b678de60ef89ad942",
+"index.html": "7c2fc81f8a71bc9143352d102358c3d7",
+"/": "7c2fc81f8a71bc9143352d102358c3d7",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "4f23bc0f042e4ee9772e8ef2353c4599",
+"main.dart.js": "995428577b9a1d711fc5bf26c619058b",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
