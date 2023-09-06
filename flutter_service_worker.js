b2e5f0cface09d2f137d09816012b97d2cd39a35
kalisynth <cade2002@gmail.com>
2023-09-06 10:19:19 +1000

    todday test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1377,10 +1377,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "398ef60bbf37050173a6e65a42d8973d",
-"/": "398ef60bbf37050173a6e65a42d8973d",
+"index.html": "e61e0f70e4ccfd28e424f2c0b28e5ab6",
+"/": "e61e0f70e4ccfd28e424f2c0b28e5ab6",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "8567db503239b73ac4ec13cca1c872ec",
+"main.dart.js": "378ec3b0461890e84905f89382bcf3a2",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
