029de463a270ed5ed7aaaf8bd4032f6e773bad13
kalisynth <cade2002@gmail.com>
2024-04-03 15:37:49 +1000

    beta update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "7eb80eff05985a1f4c7e638961d6d565",
-"assets/AssetManifest.bin.json": "b6858b68e8d954e8c0e332ccb24ed4b1",
-"assets/AssetManifest.json": "9a5693710b2daea4a8f4ace99bf52eff",
+const RESOURCES = {"assets/AssetManifest.bin": "a630c7550aec7d4bd4f602bc9fa4c4f5",
+"assets/AssetManifest.bin.json": "e00b25344148da7eac67f789a044c45c",
+"assets/AssetManifest.json": "b56253f114d89bc6f96020874fc473a1",
 "assets/assets/app_strings.dart": "d41d8cd98f00b204e9800998ecf8427e",
 "assets/assets/img/avatars/CSO/andrea.jpg": "4ff9f6f8d40ab794ebad03de00d8cbff",
 "assets/assets/img/avatars/CSO/jacinta.jpg": "545f79449473b53066a2b5afe6643e41",
@@ -412,6 +412,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "7eb80eff05985a1f4c7e638961d6d565
 "assets/assets/img/timetable/weekly/in2painttile.png": "8a42b71f79e9d681c0ba2ba3a30d6169",
 "assets/assets/img/timetable/weekly/lunchandlearntile.png": "52892395dba8b5449eadc424f6d94518",
 "assets/assets/img/timetable/weekly/meditiation.png": "b03b9a1ca837d888603c702ae9a2e249",
+"assets/assets/img/timetable/weekly/menhour.png": "b9ace4e725f78c4bec2a40ed81c43d88",
+"assets/assets/img/timetable/weekly/minecraft.png": "7060002c30f4c6f4c1d01bf3ba306058",
 "assets/assets/img/timetable/weekly/receipes.png": "83a035f11ce01220220b5d218bbe53eb",
 "assets/assets/img/timetable/weekly/shedstxt.png": "17b53e086963751e80d9b23a6ddc06f8",
 "assets/assets/img/timetable/weekly/singingtxt.png": "563f422449ffc598a5b11c1f6d75078a",
@@ -467,10 +469,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "7eb80eff05985a1f4c7e638961d6d565
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "e7700c7a3dff85e4b949dfd029305713",
-"/": "e7700c7a3dff85e4b949dfd029305713",
+"index.html": "eee2f93c0950360f700b2ef2abac4c3b",
+"/": "eee2f93c0950360f700b2ef2abac4c3b",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "6cac01ad4f044480a83cc877c192d07a",
+"main.dart.js": "5d0ebd265b2418344d09c6008cb9af52",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"};
 // The application shell files that are downloaded before a service worker can
